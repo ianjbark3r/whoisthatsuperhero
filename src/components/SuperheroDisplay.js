7816fad2b541ea1799f946e11af8103ba4b5a13c
ianjbark3r <ianjbark3r@gmail.com>
2018-11-20 20:52:00 -0600

    Added superhero display functionality

--- src/components/SuperheroDisplay.js
+++ src/components/SuperheroDisplay.js
@@ -1,12 +1,92 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const InfoDisplay = styled.div`
+  padding-bottom: 10vh;
+  padding-top: 5vh;
+  h1, h2 {
+    font-family: 'Oswald', sans-serif;
+  }
+`
+
+const InvalidHero = styled.div`
+  padding-bottom: 10vh;
+  padding-top: 5vh;
+  text-align: center;
+`
 
 export default class SuperheroDisplay extends Component {
   render() {
     if (!this.props.submitted) {
       return <div></div>
+    } else if (!this.props.heroInfo.data.results[0]) {
+      return (
+        <InvalidHero className="container">
+          <h1>Invalid hero!</h1>
+        </InvalidHero>
+      )
+    } else {
+      const { attributionText } = this.props.heroInfo
+      const { name, description, thumbnail, comics, series, events, urls } = this.props.heroInfo.data.results[0]
+      const imgSrc = `${thumbnail.path}.${thumbnail.extension}`
+      const imgAlt = `Photo of ${name}`
+
+      return (
+        <InfoDisplay className="container">
+          <div className="row">
+            <div className="col-sm">
+              <img src={imgSrc} alt={imgAlt} className="img-thumbnail" />
+            </div>
+            <div className="col-sm">
+              <h1 className="display-1">{name}</h1>
+              <hr />
+              <p style={{ marginBottom: "1rem" }}><a href="http://marvel.com">{attributionText}</a></p>
+              <p>{description}</p>
+              <hr />
+              <h2>Comics</h2>
+              <p>Here is a list of comics in which {name} makes an appearance.</p>
+              <ul>
+                {comics.items.map((comic, index) => {
+                  return (
+                    <li>{comic.name}</li>
+                  )
+                })}
+              </ul>
+              <hr />
+              <h2>Series</h2>
+              <p>Here is a list of comic series in which {name} makes an appearance.</p>
+              <ul>
+                {series.items.map((seriesEntry, index) => {
+                  return (
+                    <li key={index}>{seriesEntry.name}</li>
+                  )
+                })}
+              </ul>
+              <hr />
+              <h2>Events</h2>
+              <p>Here is a list of events in which {name} has been involved.</p>
+              <ul>
+                {events.items.map((event, index) => {
+                  return (
+                    <li key={index}>{event.name}</li>
+                  )
+                })}
+              </ul>
+              <hr />
+              <h2>Links</h2>
+              <p>Helpful links to more information regarding {name}</p>
+                {urls.map((url, index) => {
+                  return (
+                    <a href={url.url}>
+                      <button className="btn btn-outline-info" key={index}>{url.type}</button>
+                    </a>
+                  )
+                })}
+            </div>
+          </div>
+          <hr />
+        </InfoDisplay>
+      )
     }
-    return (
-      <div>{console.log(this.props.heroInfo)}</div>
-    )
   }
 }
--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -5,18 +5,18 @@ const EndCap = styled.div`
   text-align: right;
   background-color: black;
   bottom: 0;
-  color: white;
-  padding: 1rem;
+  padding: 0.5rem 1rem;
   position: fixed;
-  width: 100%;
+  a {
+    color: white;
+  }
 `
 
 export default class Footer extends Component {
   render() {
     return (
       <EndCap>
-        <div>Created by Ian J Barker</div>
-        <div><a href="http://ianjbarker.com">ianjbarker.com</a></div>
+        <div><a href="http://ianjbarker.com">Created by Ian J Barker</a></div>
       </EndCap>
     )
   }
